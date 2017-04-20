c1fb2cf9e5b9b0cd279e15046bca0393e1bdf813
Seth A. Johnson <sethajoh@umich.edu>
2017-04-20 13:11:47 -0400

    initial commit (last test doesn't pass)

--- ruby/lib/checkout.rb
+++ ruby/lib/checkout.rb
@@ -1,12 +1,41 @@
 class Checkout
+  attr_accessor :sum, :tally
   def initialize(pricing_rules)
+    @sum = 0
+    @tally = {}
   end
 
   def scan(item)
+    if @tally[item]
+      @tally[item] += 1
+    else
+      @tally[item] = 1
+    end
+    @sum += case item
+      when 'A'
+        50
+      when 'B'
+        30
+      when 'C'
+        20
+      when 'D'
+        15
+      else
+        0
+    end
   end
 
   def total
-    0
+    @tally.each do |k,v|
+      case k
+      when 'A'
+        @sum -= 20 * (v / 3).floor if v >= 3
+        
+      when 'B'
+        @sum -= 15 * (v / 2).floor if v >= 2
+      end
+    end
+    @sum
   end
 end
 
@@ -16,4 +45,3 @@ RULES = [
   {sku: 'C', price: 20, special_quantity: nil, special_price: nil},
   {sku: 'D', price: 15, special_quantity: nil, special_price: nil},
 ]
-
