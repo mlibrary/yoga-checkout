43bfe8e30e53b48eee6314511bc5f0ca8813c827
Colin Gross <grosscol@gmail.com>
2017-04-20 12:59:48 -0400

    Implement hash based methods to pass cashier specs. authors: bentoby & grosscol

--- ruby/lib/checkout.rb
+++ ruby/lib/checkout.rb
@@ -1,12 +1,50 @@
+require 'pry'
+
 class Checkout
+
+  attr :cart, :rules 
+
   def initialize(pricing_rules)
+    @rules = index_rules(pricing_rules)
+    @cart = []
   end
 
   def scan(item)
+    @cart << item 
   end
 
   def total
-    0
+    val = 0
+    counted = count_cart(@cart)
+    counted.each do |item, qt|
+      if @rules[item][:spec_qt] && qt >= @rules[item][:spec_qt]
+        val = val + qt % @rules[item][:spec_qt] * @rules[item][:price]
+        val = val + qt / @rules[item][:spec_qt] * @rules[item][:spec_price]
+      else
+        val = val + qt * @rules[item][:price]
+      end
+    end
+    return val
+  end
+
+  # Make a hash of sku and quantity
+  def count_cart(cart)
+    gb = cart.group_by {|item| item}
+    mapped = gb.map{|k,v| [k,v.count] }
+    Hash[mapped]
+  end
+
+  # Hash of hashes 
+  # {sku: {price: xx, spec_qt: x, spec_price: x}}
+  # rules[sku] = {price: xx, spec_qt: x, spec_price: x}
+  def index_rules(rules)
+    indexed = Hash.new
+    rules.each do |rule|
+      indexed[rule[:sku]] = {price: rule[:price], 
+                             spec_qt: rule[:special_quantity],
+                             spec_price: rule[:special_price]}
+    end
+    return indexed
   end
 end
 
