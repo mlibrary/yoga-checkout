21ed861a0f07de7135b3a7f97c9659594d438fa6
Matthew Alexander LaChance <mattlach@umich.edu>
2017-04-20 13:12:56 -0400

    :muscle: I don't even know ruby but this looks like it works

--- ruby/lib/checkout.rb
+++ ruby/lib/checkout.rb
@@ -1,12 +1,56 @@
+class Price
+  def initialize(price)
+    @price = price
+    @deal_price = nil
+    @deal_quantity = nil
+  end
+
+  def add_deal(price, quantity)
+    @deal_price = price
+    @deal_quantity = quantity
+  end
+
+  def total(quantity)
+    if @deal_quantity.nil?
+      @price * quantity
+
+    else
+      deals = quantity / @deal_quantity
+      extra = quantity % @deal_quantity
+
+      (deals * @deal_price) + (extra * @price)
+    end
+  end
+end
+
 class Checkout
   def initialize(pricing_rules)
+    @totals = { }
+    @prices = { }
+
+    pricing_rules.each do |item|
+      @prices[item[:sku]] = Price.new(item[:price])
+      @totals[item[:sku]] = 0
+
+      unless item[:special_quantity].nil?
+        @prices[item[:sku]].add_deal(item[:special_price],
+                                     item[:special_quantity])
+      end
+    end
   end
 
   def scan(item)
+    @totals[item] += 1
   end
 
   def total
-    0
+    result = 0
+
+    @totals.each do |item, quantity|
+      result += @prices[item].total(quantity)
+    end
+
+    result
   end
 end
 
